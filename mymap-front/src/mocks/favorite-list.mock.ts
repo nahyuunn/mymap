c794b49e7015b8fc6ac7ea4bbcf9080ad9fbbb0b
nahyuunn <138644012+nahyuunn@users.noreply.github.com>
2025-02-02 15:39:32 +0900

    (#4) Favolite-list-mock 추가

--- mymap-front/src/mocks/favorite-list.mock.ts
+++ mymap-front/src/mocks/favorite-list.mock.ts
@@ -1,12 +1,12 @@
 const favoriteListMock = [
     {
         id: "id1",
-        nickname: "user1",
+        nickname: "안녕하세요닉네임입니다",
         profileImage: null
     },
     {
         id: "id1",
-        nickname: "user2",
+        nickname: "안녕하세요닉네임입니다",
         profileImage: null
     },
     {
@@ -22,6 +22,26 @@ const favoriteListMock = [
     {
         id: "id1",
         nickname: "user5",
+        profileImage: null
+    },
+    {
+        id: "id1",
+        nickname: "안녕하세요닉네임입니다",
+        profileImage: null
+    },
+    {
+        id: "id1",
+        nickname: "user7",
+        profileImage: null
+    },
+    {
+        id: "id1",
+        nickname: "user8",
+        profileImage: null
+    },
+    {
+        id: "id1",
+        nickname: "user9",
         profileImage: null
     }
 ]
